ff92682085edaeeb9b9881c2a1007a2e59665fd6
Arthur Monteiro <amonteirom96@gmail.com>
2021-01-06 22:38:42 -0300

    Organizing backend code

--- app/controller/FormController.php
+++ app/controller/FormController.php
@@ -1,55 +1,66 @@
 <?php
 include '../model/Connection.php';
+/**
+ * validation 1: Only save files with extesion: pdf, doc, docx, odt ou txt
+ */
 $jsonReturn = [];
 
 $jsonReturn["status"] = true;
 $jsonReturn["message"] = "Recebemos sua mensagem, logo menos entraremos em contato com você.";
 
+try {
+    validateForm();
 
+    $sqlCreate = createSqlSave();
+    $stmt = $pdo->prepare($sqlCreate);
+    $stmt->bindValue(':name', $_POST["name"]);
+    $create = $stmt->execute();
 
-$validate = validateForm($_POST, $_FILES);
-if ($validate) {
-} else {
-    $jsonReturn["status"] = false;
-}
+    function saveLocalFile()
+    {
+    }
 
+    function validateForm()
+    {
+        $inputRequired = [
+            "name",
+            "email",
+            "phone",
+            "message",
+            "clientIp",
+            "file",
+        ];
 
+        foreach ($inputRequired as $nameInput) {
+            if ($nameInput == "file") {
+                if (empty($_FILES))
+                    throw new Exception("Campo de arquivo é obrigatório");
 
-function validateForm($form = [], $file)
-{
-    $inputRequired = [
-        "name",
-        "email",
-        "phone",
-        "message",
-        "clientIp",
-        "file",
-    ];
+                saveLocalFile();
+            } else {
+                if (in_array($nameInput, $_POST)) {
+                    if (empty($_POST[$nameInput])) {
+                        throw new Exception("Campo de {$_POST[$nameInput]} é obrigatório");
+                    }
 
-    foreach ($inputRequired as $nameInput) {
-        if ($nameInput == "file") {
-            if (empty($file)) {
-                $jsonReturn["message"] = "Campo de arquivo é obrigatório";
-                return false;
-            }
-        } else {
-            if (in_array($nameInput, $form)) {
-                if (empty($form[$nameInput])) {
-                    $jsonReturn["message"] = "Campo de {$form[$nameInput]} é obrigatório";
-                    return false;
+                    $_POST[$nameInput] = checkIfTrustedText($_POST[$nameInput]);
                 }
-
-                $_POST[$nameInput] = checkIfTrustedText($_POST[$nameInput]);
             }
-
         }
     }
-    return true;
-}
 
-function checkIfTrustedText($text)
-{
-    return preg_replace('/[^[:alpha:]_]/', '',$text);
+    function createSqlSave()
+    {
+    }
+
+    function checkIfTrustedText($text)
+    {
+        return preg_replace('/[^[:alpha:]_]/', '', $text);
+    }
+} catch (\Exception $e) {
+    $jsonReturn["status"] = false;
+    $jsonReturn["message"] = $e->getMessage();
 }
 
 echo json_encode($jsonReturn);
+die;
